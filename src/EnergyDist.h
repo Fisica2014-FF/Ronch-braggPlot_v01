a20a34d66ddfac55613043176f67679a4773547b
Fisica2014-FF <directoryh22@gmail.com>
2016-01-15 10:59:55 +0100

    Finito, ma non funziona... Fa NaN nei grafici

--- src/EnergyDist.h
+++ src/EnergyDist.h
@@ -1,7 +1,16 @@
 #ifndef EnergyDist_h
 #define EnergyDist_h
 
-... include header files ...
+//... include header files ...
+
+#include <vector>
+
+#include "AnalysisSteering.h"
+#include "Event.h"
+
+class TH1F;
+
+class BraggStatistic;
 
 class EnergyDist: public AnalysisSteering {
 
@@ -11,28 +20,30 @@ class EnergyDist: public AnalysisSteering {
   virtual ~EnergyDist();
 
   // function to be called at execution start
-  ...
+  virtual void beginJob();
   // function to be called at execution end
-  ...
+  virtual void   endJob();
   // function to be called for each event
-  ...
+  virtual void process( const Event& ev );
+
+  EnergyDist           ( const EnergyDist& x ) = delete;
+  EnergyDist& operator=( const EnergyDist& x ) = delete;
 
  private:
 
   // Bragg curve information
-  struct ... {
-    ...  // mean and rms energies
-    ...  // energy loss graph
+  struct BraggCurve {
+	  BraggStatistic* bragg_statistic;  // mean and rms energies
+      TH1F* braggStat_graph;  // energy loss graph
   };
 
   // set of Bragg curves for different total energies
-  ...
+  std::vector<BraggCurve*> vbraggCurv;
 
   // create objects for a Bragg curve
   void bCreate( int id, float min, float max );
 
-  EnergyDist           ( const EnergyDist& x );
-  EnergyDist& operator=( const EnergyDist& x );
+
 
 };
 
--- src/bP_01_main.cpp
+++ src/bP_01_main.cpp
@@ -6,22 +6,62 @@
 //============================================================================
 
 
-#include <iostream>
-#include <memory>
+#include <string>
 #include <vector>
-using namespace std;
+
+#include "dbg_macro.h"
+#include "EnergyDist.h"
+#include "Event.h"
+#include "EventDump.h"
+#include "EventReadFromFile.h"
+
+int main(int argc, char* argv[]) {
+	using namespace std;
+
+	// create data source
+	EventSource* es;
+	DBG(std::string filename = "./bragg_events.txt";
+		, std::string filename = argv[1];)
+	es = new EventReadFromFile(filename);
+
+	// create a list of analyzers
+	vector<AnalysisSteering*> aList;
+
+	// create object to dump event
+	// and store into list of analyzers
+	aList.push_back(new EventDump());
+
+	// create object to compute mean and rms energies
+	// and store into list of analyzers
+	aList.push_back(new EnergyDist());
+
+	// initialize all analyzers
+	for (auto as : aList) {
+		as->beginJob();
+	}
+
+	// loop over events
+	const Event* ev;
+	while ((ev = es->get())) {
+		for (auto as : aList) {
+			as->process(*ev);
+		}
+		delete ev;
+	}
+
+	// finalize all analyzers
+	for (auto as : aList) {
+		as->endJob();
+	}
+
+	//Clear memory
+	for (auto& as : aList) {
+		delete as;
+	}
+	delete es;
 
 
-
-// PROGETTO PENSATO PER SALVARE TUTTE LE VARIE OPZIONI POSSIBILI
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	std::unique_ptr<double> p(new double(4));
-	std::vector<double> v = {1,2,3};
-
-
-	cout << *p;
-	cout << v[1];
 	return 0;
 }
+
 
